1d01cebc8448af7cb62a88e3cf65115555de5754
NikhilD1998 <nikhild.dev.1998@gmail.com>
2024-05-27 14:00:48 +0530

    switching to userscreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:pdf_viewer/screens/username_Screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    //navigate to userscreen after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const UsernameScreen(),
+        ),
+      );
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF3C3C3C),
